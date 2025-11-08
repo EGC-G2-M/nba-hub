693cb94cd65eaf4c2ce3c2d20740ea57b8f66078
Marco2026 <marherluj@alum.us.es>
2025-11-08 20:46:19 +0100

    feat: zenodo removed from create_dataset

--- app/modules/fakenodo/services.py
+++ app/modules/fakenodo/services.py
@@ -1,7 +1,71 @@
 from app.modules.fakenodo.repositories import FakenodoRepository
 from core.services.BaseService import BaseService
+from app.modules.dataset.models import DataSet
+from app.modules.featuremodel.models import FeatureModel
 
 
 class FakenodoService(BaseService):
     def __init__(self):
         super().__init__(FakenodoRepository())
+
+    def create_new_deposition(self, dataset: DataSet) -> dict:
+        metadata = {
+            "title": dataset.ds_meta_data.title,
+            "upload_type": "dataset" if dataset.ds_meta_data.publication_type.value == "none" else "publication",
+            "publication_type": (
+                dataset.ds_meta_data.publication_type.value
+                if dataset.ds_meta_data.publication_type.value != "none"
+                else None
+            ),
+            "description": dataset.ds_meta_data.description,
+            "creators": [
+                {
+                    "name": author.name,
+                    **({"affiliation": author.affiliation} if author.affiliation else {}),
+                    **({"orcid": author.orcid} if author.orcid else {}),
+                }
+                for author in dataset.ds_meta_data.authors
+            ],
+            "keywords": (
+                ["uvlhub"] if not dataset.ds_meta_data.tags else dataset.ds_meta_data.tags.split(", ") + ["uvlhub"]
+            ),
+            "access_right": "open",
+            "license": "CC-BY-4.0",
+        }
+
+        data = {"metadata": metadata}
+
+        response = data
+        response['status_code'] = '200'
+        return response.json()
+
+    def upload_file(self, dataset: DataSet, deposition_id: int, feature_model: FeatureModel, user=None) -> dict:
+        """
+        Upload a file to a deposition in Zenodo.
+
+        Args:
+            deposition_id (int): The ID of the deposition in Zenodo.
+            feature_model (FeatureModel): The FeatureModel object representing the feature model.
+            user (FeatureModel): The User object representing the file owner.
+
+        Returns:
+            dict: The response in JSON format with the details of the uploaded file.
+        """
+        uvl_filename = feature_model.fm_meta_data.uvl_filename
+        data = {"name": uvl_filename}
+        # user_id = current_user.id if user is None else user.id
+        # file_path = os.path.join(uploads_folder_name(), f"user_{str(user_id)}", f"dataset_{dataset.id}/", uvl_filename)
+        # files = {"file": open(file_path, "rb")}
+
+        # publish_url = f"{self.ZENODO_API_URL}/{deposition_id}/files"
+        # response = requests.post(publish_url, params=self.params, data=data, files=files)
+        response = data
+        response['status_code'] = 201
+        return response.json()
+
+    def publish_deposition(self, deposition_id: int) -> dict:
+        # publish_url = f"{self.ZENODO_API_URL}/{deposition_id}/actions/publish"
+        # response = requests.post(publish_url, params=self.params, headers=self.headers)
+        response = {'status_code' : 202}
+        return response.json()
+
