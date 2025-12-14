fd05af97bb5fdba332106058872ae38d482f03bc
Marco2026 <marherluj@alum.us.es>
2025-12-14 19:38:42 +0100

    fix(explore): fixed selenium test to explore by extra field
    Closes #26

--- app/modules/explore/tests/test_selenium.py
+++ app/modules/explore/tests/test_selenium.py
@@ -1,46 +1,32 @@
 import pytest
 import time
+import json
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-from core.environment.host import get_host_for_selenium_testing
-from core.selenium.common import close_driver, initialize_driver
 
-class TestFilterByExtraField:
-    
-    def setup_method(self, method):
-        self.driver = initialize_driver()
-        self.host = get_host_for_selenium_testing()
-
-    def teardown_method(self, method):
-        close_driver(self.driver)
-
-    def wait_for_page_to_load(self, timeout=4):
-        """Helper para esperar a que la página cargue completamente."""
-        WebDriverWait(self.driver, timeout).until(
-            lambda driver: driver.execute_script("return document.readyState") == "complete"
-        )
-
-    def test_filterByExtraField(self):
-        driver = self.driver
-        host = self.host
-        
-        driver.get(f"{host}/")
-        self.wait_for_page_to_load()
-        
-        driver.set_window_size(706, 923)
-        driver.find_element(By.CSS_SELECTOR, ".sidebar-toggle").click()
-        driver.find_element(By.CSS_SELECTOR, ".sidebar-item:nth-child(4) .align-middle:nth-child(2)").click()
-        self.wait_for_page_to_load()
-        
-        query_input = driver.find_element(By.ID, "query")
-        query_input.click()
-        query_input.send_keys("Steals")
-        
-        time.sleep(1) 
-        
-        driver.find_element(By.LINK_TEXT, "Spurs Ring Winners").click()
-        self.wait_for_page_to_load()
-        
-        print("Test FilterByExtraField passed!")
+class TestSearchByExtraField():
+  def setup_method(self, method):
+    self.driver = webdriver.Firefox()
+    self.vars = {}
+  
+  def teardown_method(self, method):
+    self.driver.quit()
+  
+  def test_search_by_extra_field(self):
+    self.driver.get("http://localhost:5000/")
+    self.driver.set_window_size(1850, 1053)
+    self.driver.find_element(By.CSS_SELECTOR, ".sidebar-item:nth-child(4) .align-middle:nth-child(2)").click()
+    element = self.driver.find_element(By.CSS_SELECTOR, ".active .align-middle:nth-child(2)")
+    actions = ActionChains(self.driver)
+    actions.move_to_element(element).perform()
+    element = self.driver.find_element(By.CSS_SELECTOR, "body")
+    actions = ActionChains(self.driver)
+    self.driver.get("http://localhost:5000/explore?query=Steals")
+    time.sleep(2)
+    self.driver.find_element(By.LINK_TEXT, "Spurs Ring Winners").click()
