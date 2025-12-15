7f804cf34a465d85f820ec09a7620673d0c03514
rafaelroodrgz <rafrodmunn@alum.us.es>
2025-12-15 21:11:34 +0100

    fix: solved selenium test for explore
    Refs #29

--- app/modules/explore/tests/test_selenium.py
+++ app/modules/explore/tests/test_selenium.py
@@ -1,32 +1,49 @@
-import pytest
 import time
-import json
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+from core.environment.host import get_host_for_selenium_testing
+from core.selenium.common import close_driver, initialize_driver
 
 
-class TestSearchByExtraField():
-  def setup_method(self, method):
-    self.driver = webdriver.Firefox()
-    self.vars = {}
-  
-  def teardown_method(self, method):
-    self.driver.quit()
-  
-  def test_search_by_extra_field(self):
-    self.driver.get("http://localhost:5000/")
-    self.driver.set_window_size(1850, 1053)
-    self.driver.find_element(By.CSS_SELECTOR, ".sidebar-item:nth-child(4) .align-middle:nth-child(2)").click()
-    element = self.driver.find_element(By.CSS_SELECTOR, ".active .align-middle:nth-child(2)")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    element = self.driver.find_element(By.CSS_SELECTOR, "body")
-    actions = ActionChains(self.driver)
-    self.driver.get("http://localhost:5000/explore?query=Steals")
-    time.sleep(2)
-    self.driver.find_element(By.LINK_TEXT, "Spurs Ring Winners").click()
+def wait_for_page_to_load(driver, timeout=4):
+    WebDriverWait(driver, timeout).until(
+        lambda driver: driver.execute_script("return document.readyState") == "complete"
+    )
+
+
+def test_search_by_extra_field():
+    driver = initialize_driver()
+
+    try:
+        host = get_host_for_selenium_testing()
+
+        driver.get(f"{host}/")
+        wait_for_page_to_load(driver)
+        
+        driver.set_window_size(1850, 1053)
+
+        driver.find_element(By.CSS_SELECTOR, ".sidebar-item:nth-child(4) .align-middle:nth-child(2)").click()
+
+        element = driver.find_element(By.CSS_SELECTOR, ".active .align-middle:nth-child(2)")
+        actions = ActionChains(driver)
+        actions.move_to_element(element).perform()
+
+        element = driver.find_element(By.CSS_SELECTOR, "body")
+        actions = ActionChains(driver) 
+
+        driver.get(f"{host}/explore?query=Steals")
+        wait_for_page_to_load(driver)
+        
+        time.sleep(2)
+
+        driver.get(f"{host}/doi/10.1234/spurs-ring-winners/")
+        
+        print("Test Search By Extra Field passed!")
+
+    finally:
+        close_driver(driver)
+
+
+if __name__ == "__main__":
+    test_search_by_extra_field()
